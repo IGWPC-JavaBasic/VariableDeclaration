f6f6ecf5ad37cb25218f6e41448d4858b72931f7
IGWPC-JavaBasic <htun@osfl.ac.jp>
2023-02-14 16:53:37 +0900

    Touch introl

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // String variable
+        String msg = "Hello World";
+        System.out.println(msg);
+
+        /*
+         * Number
+         *  No Decimal Number
+         *      - byte, short, int, long
+         *  Decimal Number
+         *      - float, double
+         */
+
+        // can't accept decimal number
+        int intVar = 10;
+        System.out.println(intVar);
+
+        // decimal value ends with sign (f or F)
+        float floatVar = 10.9F;
+        System.out.println(floatVar);
+
+        // surrounded with single quotes
+        char charVar = 'A';
+        System.out.println(charVar);
+
+        boolean booleanVar = true;
+        System.out.println(booleanVar);
     }
 }
